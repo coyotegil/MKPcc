2ad53373a286a0bdd5973c2db9a120e430b0d677
João Gilberto Sanzovo <coyote.gil@gmail.com>
2017-09-27 17:04:14 -0300

    Initial Commit

---
+++ MKPcc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MKPcc
+//
+//  Created by João Gilberto Sanzovo on 27/09/2017.
+//  Copyright © 2017 USP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
